878cac5543fa2bf3ad3afda9bdceb4d2d3975f40
francis-njoku <njokuchimauche@gmail.com>
2023-12-12 08:11:18 +0100

    2nd commit

---
+++ resources/views/qrCode.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>How to Generate QR Code in Laravel 8? - raviyatechnical</title>
+</head>
+
+<body>
+    <div class="visible-print text-center">
+        <h1>Laravel 8 - QR Code Generator Example</h1>
+        {!! QrCode::size(250)->generate('raviyatechnical') !!}
+        <p>example by raviyatechnical.</p>
+    </div>
+</body>
+
+</html>
