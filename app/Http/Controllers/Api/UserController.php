dadbdc2ba21c7a9abfacc6ffa889459bdc1c8517
francis-njoku <njokuchimauche@gmail.com>
2023-12-30 05:50:01 +0100

    Add forgot password

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -3,13 +3,21 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\ForgotPasswordRequest;
 use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Auth\Events\PasswordReset;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Auth\Events\Verified;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Resources\Json\JsonResource;
+use App\Mail\ResetPassword;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Mail;
 
 class UserController extends Controller
 {
@@ -107,4 +115,118 @@ class UserController extends Controller
             ], 500);
         }
     }
+
+    /**
+     * @return JsonResponse
+     */
+    public function logout(): JsonResource
+    {
+        auth()->user()->tokens()->delete();
+
+        return response()->success([], 'logged out', 200);
+    }
+
+    /**
+     * @param ForgotPasswordRequest $request
+     * @return JsonResponse
+     * @throws ValidationException
+     */
+    public function forgotPassword(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'email' => ['required', 'string', 'email', 'max:255'],
+        ]);
+
+        if ($validator->fails()) {
+            return new JsonResponse(['success' => false, 'message' => $validator->errors()], 422);
+        }
+
+        $verify = User::where('email', $request->all()['email'])->exists();
+
+        if ($verify) {
+            $verify2 = DB::table('password_resets')->where([
+                ['email', $request->all()['email']]
+            ]);
+
+            if ($verify2->exists()) {
+                $verify2->delete();
+            }
+
+            $token =  random_int(100000, 999999);
+            $password_reset = DB::table('password_resets')->insert([
+                'email' => $request->all()['email'],
+                'token' => $token,
+                'created_at' => Carbon::now()
+            ]);
+
+            if ($password_reset) {
+                Mail::to($request->all()['email'])->send(new ResetPassword($token));
+
+                return new JsonResponse(
+                    [
+                        'success' => true,
+                        'message' => "Please check your email for a 6 digit pin"
+                    ],
+                    200
+                );
+            } else {
+                return new JsonResponse(
+                    [
+                        'success' => false,
+                        'message' => "This email does not exist"
+                    ],
+                    400
+                );
+            }
+        }
+    }
+
+    /**
+     * @param ForgotPasswordRequest $request
+     * @return JsonResponse
+     * @throws ValidationException
+     */
+
+    /**
+    public function forgot(ForgotPasswordRequest $request): JsonResource
+    {
+        
+        $user = ($query = User::query());
+
+        $user = $user->where($query->qualifyColumn('email'), $request->input('email'))->first();
+
+        // if no such user exists then throw an error
+        if (!$user || !$user->email) {
+            return response()->error('No Record Found', 'Incorrect Email Address Provided', 404);
+        }
+
+        // Generate a 4 digit random Token
+        $resetPasswordToken = str_pad(random_int(1, 9999), 4, '0', STR_PAD_LEFT);
+
+        // In case User has already requested for forgot password don't create another record
+        // Instead update the existing token with the new token
+        if (!$userPassReset = PasswordReset::where('email', $user->email)->first()) {
+            // Store TOken in DB with Token Expiration Time ié: 1 hour
+            PasswordReset::create([
+                'email' => $user->email,
+                'token' => $resetPasswordToken,
+            ]);
+        } else {
+            // Store Token in DB with Token expiration time 1 hour
+            $userPassReset->update([
+                'email' => $user->email,
+                'token' => $resetPasswordToken,
+            ]);
+        }
+        // Send notification to the user about the reset token
+        $user->notify(
+            new PasswordResetNotification(
+                $user,
+                $resetPasswordToken
+            )
+        );
+
+        return new JsonResponse(['message' => 'A Code has been Sent to your Email Address.']);
+    }
+     */
 }
