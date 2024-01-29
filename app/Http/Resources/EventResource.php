2d822af95eac2698a5d8985938150458b1bd72c4
francis-njoku <njokuchimauche@gmail.com>
2024-01-29 01:06:02 +0100

    Create resources

---
+++ app/Http/Resources/EventResource.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\URL;
+
+class EventResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'eventTypeId' => $this->eventTypeId,
+            'createdBy' => $this->createdBy,
+            'slug' => $this->slug,
+            'status' => !!$this->status,
+            'location' => $this->location,
+            'excerpt' => $this->excerpt,
+            'description' => $this->description,
+            'start' => (new \DateTime($this->start))->format('Y-m-d H:i:s'),
+            'end' => (new \DateTime($this->end))->format('Y-m-d H:i:s'),
+            'created_at' => (new \DateTime($this->created_at))->format('Y-m-d H:i:s'),
+            'updated_at' => (new \DateTime($this->updated_at))->format('Y-m-d H:i:s'),
+        ];
+    }
+}
