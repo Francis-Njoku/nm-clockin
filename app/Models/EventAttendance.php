58537c9c9f57dd7b840e977352654d7075ab7225
Francis Xavier <njokuchimauche@gmail.com>
2024-04-05 20:59:50 +0100

    update models

--- app/Models/EventAttendance.php
+++ app/Models/EventAttendance.php
@@ -8,5 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class EventAttendance extends Model
 {
     use HasFactory;
+    
     protected $fillable = ['userId', 'eventId', 'attend'];
 }
--- app/Models/EventGallery.php
+++ app/Models/EventGallery.php
@@ -8,5 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class EventGallery extends Model
 {
     use HasFactory;
-    protected $fillable = ['eventId', 'image', 'isFeatured'];
+    
+    protected $fillable = ['image', 'isFeatured', 'eventId'];
 }
