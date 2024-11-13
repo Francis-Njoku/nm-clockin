7229ee6e586542828c5f3a73670a5466586cc2fa
Francis Xavier <njokuchimauche@gmail.com>
2024-11-13 21:39:56 +0100

    make leave

---
+++ app/Models/LeaveUser.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class LeaveUser extends Model
+{
+    use HasFactory;
+    protected $fillable = ['user_id', 'leave_id'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function leave()
+    {
+        return $this->belongsTo(Leave::class);
+    }
+
+    
+}
--- database/migrations/2024_11_13_093654_create_leave_users_table.php
+++ database/migrations/2024_11_13_093654_create_leave_users_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateLeavesTable extends Migration
+class CreateLeaveUsersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,16 +13,10 @@ class CreateLeavesTable extends Migration
      */
     public function up()
     {
-        Schema::create('leaves', function (Blueprint $table) {
+        Schema::create('leave_users', function (Blueprint $table) {
             $table->id();
             $table->foreignIdFor(\App\Models\User::class, 'user_id');
-            $table->foreignIdFor(\App\Models\User::class, 'approved_by');
-            $table->string('name');
-            $table->boolean('is_approved');
-            $table->string('status');
-            $table->string('leave_type');
-            $table->date('start');
-            $table->date('end');
+            $table->foreignIdFor(\App\Models\Leave::class, 'leave_id');
             $table->timestamps();
         });
     }
@@ -34,6 +28,6 @@ class CreateLeavesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('leaves');
+        Schema::dropIfExists('leave_users');
     }
 }
